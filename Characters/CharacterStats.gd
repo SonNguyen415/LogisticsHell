05c541f56229572f838c383a6589b7ad14c26f8e
Max Kim <31351608+Solar-Flare@users.noreply.github.com>
2022-02-10 21:25:44 +0700

    Character Stat Setup

---
+++ Characters/CharacterStats.gd
@@ -0,0 +1,37 @@
+extends Resource
+class_name CharacterStats
+
+# Last Updated 2/10/22
+
+
+# General Stats
+export (int) var health
+export (int) var luck
+
+
+#Administration Stats
+export (int) var diplomacy
+export (int) var architect
+export (int) var financial_management
+export (int) var food_production
+export (int) var quartermaster
+export (int) var hospital_management
+export (int) var transportation
+export (int) var innovation
+
+
+# General Stats 
+export (int) var logistics
+export (int) var offense
+export (int) var defense
+export (int) var siege
+export (int) var maneuver
+export (int) var charisma
+export (int) var drill
+export (int) var tactics
+export (int) var retreat
+
+export (int) var cavalry_bonus
+export (int) var infantry_bonus
+export (int) var artillery
+export (String) var attribute
