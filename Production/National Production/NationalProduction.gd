f4645ca10bafc283a7d298eb529cfc3834243153
Son Nguyen <Son.mofa415@gmail.com>
2022-02-12 22:05:04 -0500

    functions

--- Production/National Production/NationalProduction.gd
+++ Production/National Production/NationalProduction.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var building_count = 0
 
+var idle_buildings = 0
 var medical_convoy_buildings = 0
 var medicine_buildings = 0
 var hygiene_buildings = 0
@@ -9,6 +10,23 @@ var food_buildings = 0
 var ammunition_buildings = 0
 var weapon_buildings = 0
 var cloth_buildings = 0
+
+
+var recruitment_center = 0
+
+
+
+func construct_building():
+	building_count += 1
+	idle_buildings += 1
+	
+	
+func allocate_building(building):
+	if(idle_buildings > 0):
+		building += 1
+	else:
+		pass
+
 
 func _ready():
 	pass # Replace with function body.
