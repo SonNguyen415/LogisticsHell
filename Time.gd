d54a32a9ec042122864ce6cfcc480591c1d89acf
Max Kim <31351608+Solar-Flare@users.noreply.github.com>
2022-02-13 10:40:07 +0700

    CommentingHell

--- Time.gd
+++ Time.gd
@@ -2,20 +2,24 @@ extends Node
 
 # Autoloaded Script
 
-
+# Number of seconds in 1 game hour
 var ticks_in_hour = 1 setget set_speed
-var seconds_per_tick = 1
 
+# Date
 var current_date = [999, 41, 1] #Year, Date, Hour
 
+# Some constants for this fictional date
 const hours_in_day = 24
 const days_in_year = 300
+
+# Stores current seconds
 var summary_delta = 0
 
 signal tick_generated()
 
 
 func set_speed(ticks_per_hour):
+	# Sets the game speed (ticks/seconds per hour)
 	self.ticks_in_hour = ticks_per_hour
 
 func _process(delta):
@@ -27,6 +31,8 @@ func _process(delta):
 		emit_signal("tick_generated")
 
 func add_hour():
+	# Increases the number of hours in current_date by 1
+	# Also makes the appropriate changes to date and year
 	current_date[2] += 1
 	while current_date[2] >= hours_in_day:
 		current_date[2] -= (hours_in_day - 1)
