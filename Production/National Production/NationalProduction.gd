9b32290fdb302b4ec441c96a48335739b8546cb1
Son Nguyen <Son.mofa415@gmail.com>
2022-02-12 22:40:17 -0500

    commentator

--- Production/National Production/NationalProduction.gd
+++ Production/National Production/NationalProduction.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 
+# Total number of buildings you have
 var building_count = 0
 
+
+# Special buildings, add up to building_count
 var idle_buildings = 0
 var medical_convoy_buildings = 0
 var medicine_buildings = 0
@@ -12,7 +15,11 @@ var ammunition_buildings = 0
 var weapon_buildings = 0
 var cloth_buildings = 0
 
+
+# This building is special cuz it affects the global resource manpower
 var recruitment_center = 0
+
+
 
 
 func construct_building():
@@ -20,6 +27,9 @@ func construct_building():
 	idle_buildings += 1
 	
 	
+	
+	
+# Call this function whenever a building is allocated to production
 func allocate_building(building):
 	if(idle_buildings > 0):
 		building += 1
@@ -28,6 +38,8 @@ func allocate_building(building):
 		pass
 		
 		
+		
+# Every tick, we increase resources, this is represented as income
 func on_tick():
 	GlobalResources.cash += GlobalResources.tax_rate
 	GlobalResources.manpower += GlobalResources.recruitment_rate*(recruitment_center+1)
