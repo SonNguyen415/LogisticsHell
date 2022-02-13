5a063d51d49c2616caabf3cedb114a1ab656b198
Max Kim <31351608+Solar-Flare@users.noreply.github.com>
2022-02-13 10:18:25 +0700

    time

--- Time.gd
+++ Time.gd
@@ -7,6 +7,9 @@ var ticks_in_hour = 1 setget set_speed
 var seconds_per_tick = 1
 
 var current_date = [999, 41, 1] #Year, Date, Hour
+
+var hours_in_day = 24
+var days_in_year = 300
 var summary_delta = 0
 func set_speed(ticks_per_hour):
 	self.ticks_in_hour = ticks_per_hour
@@ -20,3 +23,9 @@ func _process(delta):
 
 func add_hour():
 	current_date[2] += 1
+	while current_date[2] >= hours_in_day:
+		current_date[2] -= (hours_in_day - 1)
+		current_date[1] += 1
+		if current_date[1] >= days_in_year:
+			current_date[1] -= (days_in_year-1)
+			current_date[0] += 1
