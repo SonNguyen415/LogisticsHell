0b9dfe3af2fb9c318d1e7a7de8405aedfc2a3b66
Max Kim <31351608+Solar-Flare@users.noreply.github.com>
2022-02-13 10:08:34 +0700

    tick tock mfs

--- Time.gd
+++ Time.gd
@@ -6,5 +6,16 @@ extends Node
 var ticks_in_hour = 1 setget set_speed
 var seconds_per_tick = 1
 
+var current_date = [999, 41, 1] #Year, Date, Hour
+var summary_delta = 0
 func set_speed(ticks_per_hour):
 	self.ticks_in_hour = ticks_per_hour
+
+func _process(delta):
+	summary_delta += delta
+	while summary_delta >= 1:
+		summary_delta -= 1
+		add_hour()
+
+func add_hour():
+	current_date[2] += 1
