1783b56922ee6926f87dc1af850afa17aad9d9fb
Max Kim <31351608+Solar-Flare@users.noreply.github.com>
2022-02-13 10:34:18 +0700

    Update GlobalResources.gd

--- GlobalResources.gd
+++ GlobalResources.gd
@@ -16,8 +16,7 @@ const cloth_production = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Time.connect("tick_generated", self, "on_tick")
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
