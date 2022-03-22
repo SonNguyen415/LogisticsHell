83d2d19ec620fe8636e89691adc1e0ebee4b58cb
Max Kim <31351608+Solar-Flare@users.noreply.github.com>
2022-03-22 09:24:48 +0700

    I'm gonna throw a dictionary at you

--- Production/National Production/SettlementResources.gd
+++ Production/National Production/SettlementResources.gd
@@ -1,45 +1,23 @@
 extends Node
 class_name SettlementResource
 
-# Storage Count
-var food
-var wood
-var copper
-var cloth
-var iron
-var steel
-var glasses
-var gunpowder
-var preservatives
-var smokeless_powder
-var horses
-var coal
-var oil
-var electricity
-var uranium
-
-var storage = [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0]
-
-var storage_dictionary = ["food", "wood", "copper", "cloth", "iron",
-						 "steel", "glasses", "gunpowder", "preservatives", "smokeless_powder",
-						"horses", "coal", "oil", "electricity", "uranium"]
+var storage_dictionary = {"food" : 0, "wood" : 0, "copper" : 0, "cloth" : 0, "iron" : 0,
+						 "steel" : 0, "glasses": 0, "gunpowder" : 0, "preservatives" : 0, "smokeless_powder" : 0,
+						"horses" : 0, "coal" : 0, "oil" : 0, "electricity" : 0, "uranium" : 0}
 var resource_count = 15 # Total number of unique resources
 # We can add more later I guess
 
 func add_resource(resource, count):
 	# Resource: String name of resource to add
 	# Count: Amount to add
-	for i in resource_count:
-		if resource == storage_dictionary[i]:
-			storage[i] += count
-			break
+	storage_dictionary[resource] += count
 
 func subtract_resource(resource, count):
 	# Resource: String name of resource to add
 	# Returns true if successful, and false if unable to (not enough vespene gas)
-	for i in resource_count:
-		if resource == storage_dictionary[i]:
-			if storage[i] < count:
-				return false
-			storage[i] -= count
-			return true
+	if storage_dictionary[resource] < count:
+		return false
+	storage_dictionary[resource] -= count
+
+func get_resource_count(resource):
+	return storage_dictionary[resource]
