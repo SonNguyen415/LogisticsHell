09a2a69bd78875d9d0b5e9c4d46cb773e14c60a0
Max Kim <31351608+Solar-Flare@users.noreply.github.com>
2022-03-22 15:50:15 +0700

    More Reallocation

--- Production/National Production/SettlementBuildings.gd
+++ Production/National Production/SettlementBuildings.gd
@@ -1,33 +1,14 @@
 extends Node
-
-
 class_name SettlementBuildings
-
 
 const WAREHOUSE_SPACE = 1000
 const WAREHOUSE_COST = 10
-
 
 # Storage buildings
 var warehouses
 var max_capacity = warehouses*WAREHOUSE_SPACE
 var available_capacity = max_capacity
 
-
-
-# Settlement specific buildings
-var markets
-var temples
-var hospitals
-var defenses
-var watchtowers
-var barracks
-var training_fields
-var train_stations
-
-
-
-# Production buildings
 var idle_factories = 0
 var medical_convoy_factories = 0
 var medicine_factories = 0
@@ -37,20 +18,26 @@ var ammunition_factories = 0
 var weapon_factories = 0
 var cloth_factories = 0
 
+# Settlement specific buildings
+
+# Civ
+var markets
+var temples
 var farms = 0
 var metal_mine = 0
+var train_stations
 
+# Def
+var defenses
+var watchtowers
 
+# Milta
+var hospitals
+var barracks
+var training_fields
 
 func build_warehouse():
-	GlobalResources.cash -= WAREHOUSE_COST
-	warehouses += 1
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	# Builds a Warehouse (Supply Depot)
+	if GlobalResources.cash >= WAREHOUSE_COST:
+		GlobalResources.cash -= WAREHOUSE_COST
+		warehouses += 1
