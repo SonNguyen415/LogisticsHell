58abd46935e26d4662db317c0dc837b6cfc97a17
Max Kim <31351608+Solar-Flare@users.noreply.github.com>
2022-03-22 09:19:00 +0700

    Subtractive Synthesis

--- Production/National Production/SettlementResources.gd
+++ Production/National Production/SettlementResources.gd
@@ -1,6 +1,7 @@
 extends Node
 class_name SettlementResource
 
+# Storage Count
 var food
 var wood
 var copper
@@ -17,5 +18,28 @@ var oil
 var electricity
 var uranium
 
-func _ready():
-	pass # Replace with function body.
+var storage = [0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0]
+
+var storage_dictionary = ["food", "wood", "copper", "cloth", "iron",
+						 "steel", "glasses", "gunpowder", "preservatives", "smokeless_powder",
+						"horses", "coal", "oil", "electricity", "uranium"]
+var resource_count = 15 # Total number of unique resources
+# We can add more later I guess
+
+func add_resource(resource, count):
+	# Resource: String name of resource to add
+	# Count: Amount to add
+	for i in resource_count:
+		if resource == storage_dictionary[i]:
+			storage[i] += count
+			break
+
+func subtract_resource(resource, count):
+	# Resource: String name of resource to add
+	# Returns true if successful, and false if unable to (not enough vespene gas)
+	for i in resource_count:
+		if resource == storage_dictionary[i]:
+			if storage[i] < count:
+				return false
+			storage[i] -= count
+			return true
