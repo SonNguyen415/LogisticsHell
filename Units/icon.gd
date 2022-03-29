6550356700e752496929f2a2398e2841b2d34840
Warnerd <94016157+Warnerd03@users.noreply.github.com>
2022-03-29 16:12:31 -0400

    army movement done

--- Units/icon.gd
+++ Units/icon.gd
@@ -3,25 +3,40 @@ extends Sprite
 
 # Declare member variables here. Examples:
 var clicked = false;
-
+var destinationX = global_position.x
+var destinationY = global_position.y
+var movementSpeed = 10;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+#Army movement, if the distance is more than 2 times the movement speed, move
+
+func _process(delta):
+	var distance = global_position.distance_to(Vector2(destinationX, destinationY));
+	if (distance > movementSpeed*2):
+		var cosine = (destinationX-global_position.x)/distance
+		var sine = (destinationY-global_position.y)/distance
+		global_position.x += cosine*movementSpeed
+		global_position.y += sine*movementSpeed
+		
+#Player input, if you click on the army, it selects it, click on it again, deselects, right click somewhere, activates movement
+
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		if get_rect().has_point(to_local(event.position)) and clicked == false:
 			clicked = true
-			print("selected")
+			modulate = Color.red
 		elif get_rect().has_point(to_local(event.position)) and clicked == true:
 			clicked = false
-			print("deselected")
+			modulate = Color.white
 	elif event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_RIGHT:
 		if clicked == true:
-			global_position = get_global_mouse_position()
+			destinationX = get_global_mouse_position().x
+			destinationY = get_global_mouse_position().y
 			clicked = false
-			print("move")
+			modulate = Color.white
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
