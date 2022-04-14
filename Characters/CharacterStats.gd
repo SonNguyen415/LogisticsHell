edd8008058b7241cb86ad0c3c7e058a033d4e12b
OliverKris <93892018+OliverKris@users.noreply.github.com>
2022-04-13 23:41:19 -0400

    ...

--- Characters/CharacterStats.gd
+++ Characters/CharacterStats.gd
@@ -5,7 +5,7 @@ A characterstats class that stores stats of characters
 Meant to be attached to a character to represent the character's stats
 Also meant to be called to obtain said character's stats
 
-Authors: Max Kim
+Authors: Max Kim, Oliver Krisetya
 
 ---------------------------------------------------------------------------------------------------
 """
@@ -13,10 +13,10 @@ Authors: Max Kim
 extends Resource
 class_name CharacterStats
 
-# Last Updated 2/10/22
+# Last Updated 4/13/22
 
 
-# General Stats
+# Teir Stats
 export (int) var tier = 3# 3star, 4star, 5star...
 
 export (int) var health = 100
@@ -36,8 +36,6 @@ export (int) var recruitment = 10
 
 
 # General Stats 
-
-
 export (int) var logistics = 10
 export (int) var offense = 10
 export (int) var defense = 10
@@ -52,4 +50,31 @@ export (int) var cavalry_bonus = 10
 export (int) var infantry_bonus = 10
 export (int) var artillery = 10
 
+# Personality Stats
 export (String) var attribute
+
+
+#Functions
+
+
+# Randomizes and generates general stats given
+# specific high and low stats of the general
+
+func generate_gen_stats(high_stats, low_stats, tier):
+	# Base zero stats
+	var stats_dict = {"Logistics": 0,
+					 "Offense": 0,
+					 "Defense": 0,
+					 "Siege": 0,
+					 "Maneuver": 0,
+					 "Charisma": 0,
+					 "Drill": 0,
+					 "Tactics": 0,
+					 "Retreat": 0} 
+	var max_stats = preload("res://Characters/Characters/Debug/MaxStats.tres")
+	print(max_stats.new())
+	
+	return stats_dict
+
+func generate_adm_stats(high_stats, low_stats):
+	return 1
