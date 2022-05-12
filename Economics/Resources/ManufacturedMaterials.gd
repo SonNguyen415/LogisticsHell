4b5843b83b47befa1b7d33aa78ce7c1832b8a09c
Max Kim <31351608+Solar-Flare@users.noreply.github.com>
2022-05-12 10:32:18 -0400

    Resource Setup

---
+++ Economics/Resources/ManufacturedMaterials.gd
@@ -0,0 +1,10 @@
+# This is just a list of names
+# Don't worry, this is processed into a dictionary at ResourceList.tscn
+# So it's just easier to change the list of materials or whatever
+var material_names = ["Bows", "Arrows", "Musket", "Matchlock", "Arquebus", 
+					"Muzzle Rifles", "Breech Rifles", "Gatling Guns", "Bayonets", 
+					"Sabers", "Longswords", "Rapiers", "Field Artillery", "Siege Artillery",
+					"Howitzers", "Mortars", "Lances", "Pikes", "Plate Armor", "Cuirasses",
+					"Helmets", "Uniforms", "Boots", "Hygiene", "Toilet Paper", "Bandaids",
+					"Penicillin", "Gauze", "Face Masks", "MRE", "Alcohol", "Mobile Kitchen", 
+					"Canned Food", "Stone", "Bricks", "Wagons", "Boats", "Automobiles"]
