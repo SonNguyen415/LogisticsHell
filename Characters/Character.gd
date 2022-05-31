f10e53aa387c5d6c228caf302a2089e24d033fbc
OliverKris <93892018+OliverKris@users.noreply.github.com>
2022-05-30 21:22:27 -0400

    Beginning of Hero Functions

--- Characters/Character.gd
+++ Characters/Character.gd
@@ -12,8 +12,48 @@ Authors: Max Kim
 extends Resource
 class_name Character
 
-export (Resource) var stats
-export (Resource) var skill_tree
-export (Resource) var constellations
 export (String) var name
 export (String) var whatever
+export (int) var experience
+export (int) var level
+var currentLevelCap
+var tier
+var levelPoints
+
+
+
+var rng = RandomNumberGenerator.new()
+
+#functions
+
+func levelUpPoints():
+	levelPoints = levelPoints + 1
+
+func setTier(num):
+	if(num <= 160):
+		return 1
+	elif(num >= 161 && num <= 199):
+		return 2
+	elif(num == 200):
+		return 3
+	else:
+		return 0
+
+func addXP(xp):
+	experience = experience + xp
+
+func levelUp():
+	if(experience >= currentLevelCap):
+		level = level + 1
+		var temp = currentLevelCap - experience
+		experience = temp
+		levelUpPoints()
+
+
+#constructor
+
+func _init():
+	level = 1
+	
+	var tierRating = rng.randf_range(1,200)
+	tier = setTier(tierRating)
